2ea2e1247bb9ed0dc2eb717826349363814c1ddb
kron79 <ciaran.brown79@gmail.com>
2024-12-04 16:01:00 +0000

    Logo change

---
+++ .storybook/manager.ts
@@ -0,0 +1,11 @@
+import { addons } from '@storybook/manager-api';
+import { create } from '@storybook/theming/create';
+
+addons.setConfig({
+  theme: create({
+    base: 'light',
+    brandTitle: 'MetaBlueprint - Design System',
+    // brandUrl: 'https://your-website.com',
+    brandImage: '/metablueprint.svg',
+  }),
+});
